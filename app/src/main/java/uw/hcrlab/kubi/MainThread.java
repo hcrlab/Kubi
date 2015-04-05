92d8e79f69b49d5350389ed95757ebb2589e1523
Kimyen <kimyen0816@gmail.com>
2015-04-05 13:41:00 -0700

    add new MainThread

---
+++ app/src/main/java/uw/hcrlab/kubi/MainThread.java
@@ -0,0 +1,14 @@
+package uw.hcrlab.kubi;
+
+import com.revolverobotics.kubiapi.KubiManager;
+
+import uw.hcrlab.kubi.screen.OldRobotFace;
+
+/**
+ * Created by kimyen on 4/5/15.
+ */
+public class MainThread extends Thread {
+    public MainThread(OldRobotFace robotFace, KubiManager kubiManager, MainActivity mainActivity) {
+        super();
+    }
+}
