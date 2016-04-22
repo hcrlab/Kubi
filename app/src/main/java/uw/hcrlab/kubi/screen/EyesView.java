73c90e5d24e39c1f5915d51e53aabdece34d6dfb
Alex Fiannaca <fiannac4@live.com>
2016-04-21 17:55:01 -0700

    Starting to add animations in new EyesView

--- app/src/main/java/uw/hcrlab/kubi/screen/EyesView.java
+++ app/src/main/java/uw/hcrlab/kubi/screen/EyesView.java
@@ -1,38 +1,73 @@
 package uw.hcrlab.kubi.screen;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.text.TextPaint;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
 import android.view.View;
-
-import uw.hcrlab.kubi.R;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.widget.FrameLayout;
 
 /**
  * TODO: document your custom view class.
  */
 public class EyesView extends View {
 
-    public EyesView(Context context) {
-        super(context);
-    }
+    private int mScreenHeight;
+    private int mScreenWidth;
+
+    private int mRadius;
+
+    private float mLX;
+    private float mRX;
+    private float mY;
+
+    private ShapeDrawable mPupilDrawable;
 
     public EyesView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mPupilDrawable = new ShapeDrawable(new OvalShape());
+        mPupilDrawable.getPaint().setColor(0xff74AC23);
     }
 
-    public EyesView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mScreenHeight = h;
+        mScreenWidth = w;
+
+        mRadius = mScreenHeight/6;
+
+        mLX = mScreenWidth/2 - 1.25f * mRadius;
+        mRX = mScreenWidth/2 + 1.25f * mRadius;
+        mY = mRadius + 0.25f * mRadius;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        mPupilDrawable.draw(canvas);
     }
 
+    public void go() {
+        mPupilDrawable.setBounds((int)mLX - mRadius, (int)mY - mRadius, (int)mLX + 2 * mRadius, (int)mY + 2 * mRadius);
+
+        ValueAnimator anim = ValueAnimator.ofInt(0, 20);
+        anim.setInterpolator(new AnticipateOvershootInterpolator());
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int val = (Integer) valueAnimator.getAnimatedValue();
+
+                mPupilDrawable.setBounds((int)mLX - mRadius, (int)mY - mRadius, (int)mLX + 2 * mRadius, (int)mY + 2 * mRadius);
+            }
+        });
+        anim.setDuration(500);
+        anim.start();
+    }
 }
