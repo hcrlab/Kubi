088c3764f1642a5383282b45ce77fc252ea6c857
Alex Fiannaca <fiannac4@live.com>
2016-05-03 17:16:12 -0700

    Starting to add setup activity

---
+++ app/src/main/java/uw/hcrlab/kubi/SetupActivity.java
@@ -0,0 +1,13 @@
+package uw.hcrlab.kubi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+}
---
+++ app/src/main/java/uw/hcrlab/kubi/robot/Eyes.java
@@ -0,0 +1,7 @@
+package uw.hcrlab.kubi.robot;
+
+/**
+ * Created by Alexander on 5/3/2016.
+ */
+public class Eyes {
+}
